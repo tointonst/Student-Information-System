adfb4fce1a313715eb2d4b0f760f95d655c6bcc2
harris.butler <harris.butler@MU-C3-12.mullen.pvt.k12.co.us>
2014-10-21 08:30:13 -0600

    mainMenu started

---
+++ Student Information System Group Project/src/UserMenu.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class UserMenu 
+{
+	public static void mainMenu() 
+	{
+		System.out.println("Main Menu");
+		System.out.println("\t What would like to do");
+		System.out.println("\t\t 1) add or delete a student.");
+		System.out.println("\t\t 2) change student grades.");
+		System.out.println("\t\t 3) sort students.");
+		Scanner userInput1 = new Scanner(System.in);
+		int choseNumber = userInput1.nextInt();
+		
+		if(choseNumber == 1) 
+		{
+			setStudent.setStudentMenu(); 
+		}
+		if(choseNumber == 2)
+		{
+			changeGradesAndClasses.changeGrade(); 
+		}
+		if(choseNumber == 3) 
+		{
+		  	
+		}
+		else 
+		{
+		  System.out.println("Please input a corect character ex 1, 2, or 3");	
+		}
+	}
+}
