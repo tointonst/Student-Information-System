32723db1bc559b0aa95096c057bd1764ff31e218
steven.tointon <steven.tointon@MU-C3-11.mullen.pvt.k12.co.us>
2014-10-21 08:34:16 -0600

    added class sorter

--- Student Information System Group Project/src/ClassSorter.java
+++ Student Information System Group Project/src/ClassSorter.java
@@ -1,5 +1,24 @@
+import java.util.Comparator;
 
-public class ClassSorter
+public class ClassSorter //implements Comparator<Student>
 	{
+	public int compare(Student s1, Student s2, Student s3)
+		{
 
+		if (s1.getPeriodOne().compareTo(s2.getPeriodTwo()) < 0)
+			{
+			if (s2.getPeriodTwo().compareTo(s3.getPeriodThree()) < 0)
+				{
+				return s1.getPeriodOne().compareTo(s3.getPeriodThree());
+				} 
+			else
+				{
+				return s2.getPeriodTwo().compareTo(s3.getPeriodThree());
+				}
+			}
+		else 
+			{
+			return s1.getPeriodOne().compareTo(s3.getPeriodThree());
+			}
+		}
 	}
