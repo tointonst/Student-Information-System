36f4548ad4c87aed5dafc488945eb46c8d9098ff
tointonst <tointonst@mullenmail.com>
2014-10-22 10:43:53 -0600

    Update SortStudents.java

--- Student Information System Group Project/src/SortStudents.java
+++ Student Information System Group Project/src/SortStudents.java
@@ -1,44 +1,61 @@
 import java.util.Collections;
 import java.util.Scanner;
-public class SortStudents 
-{
-	public static void sortStudentsMenu() 
+
+public class SortStudents
 	{
+	static int userPeriodSort;
+	public static void sortStudentsMenu()
+		{
 		System.out.println("Sort Students");
 		System.out.println("\t 1) sort by name.");
 		System.out.println("\t 2) sort by GPA.");
 		System.out.println("\t 3) sort by class.");
-		
-		Scanner userInput2 = new Scanner(System.in); 
+
+		Scanner userInput2 = new Scanner(System.in);
 		int sortTheStudents = userInput2.nextInt();
+
+		if (sortTheStudents == 1)
+			{
+			sortByName();
+			}
+		if (sortTheStudents == 2)
+			{
+			sortByGPA();
+			}
+		if (sortTheStudents == 3)
+			{
+			sortByClass();
+			} else
+			{
+			System.out
+					.println("Please input an accepted character ex 1,2 or 3. ");
+			}
+		}
+
+	public static void sortByName()
+		{
+		Collections.sort(DataOfStudent.studentInformation, new NameSorter());
+		}
+
+	public static void sortByGPA()
+		{
+		Collections.sort(DataOfStudent.studentInformation, new GPASorter());
+		}
+
+	public static void sortByClass()
+		{
+		System.out.println("Which period would you like to sort by?");
+		System.out.println("\t 1) Period 1.");
+		System.out.println("\t 2) Peroid 2.");
+		System.out.println("\t 3) Period 3.");
+		Scanner userInput = new Scanner(System.in);
+		userPeriodSort = userInput.nextInt();
 		
-		if(sortTheStudents == 1) 
-		{
-			sortByName(); 
+		Collections.sort(DataOfStudent.studentInformation, new ClassSorter());
+		for(int i=0; i<DataOfStudent.studentInformation.size(); i++)
+			{
+		    System.out.println(DataOfStudent.studentInformation.get(i).getFirstName() + " " + DataOfStudent.studentInformation.get(i).getLastName()  + "\t" + DataOfStudent.studentInformation.get(i).getPeriodOne() + " " + DataOfStudent.studentInformation.get(i).getPeriodOneGrade() +"\t" + DataOfStudent.studentInformation.get(i).getPeriodTwo() + " " + DataOfStudent.studentInformation.get(i).getPeriodTwoGrade() +"\t" + DataOfStudent.studentInformation.get(i).getPeriodThree() + " " + DataOfStudent.studentInformation.get(i).getPeriodThreeGrade());
+		    } 
 		}
-		if(sortTheStudents == 2) 
-		{
-			sortByGPA(); 
-		}
-		if(sortTheStudents == 3) 
-		{
-			sortByClass(); 
-		}
-		else 
-		{
-			System.out.println("Please input an accepted character ex 1,2 or 3. ");
-		}
-	}
-	public static void sortByName() 
-	{
-		Collections.sort(DataOfStudent.studentInformation, new NameSorter()); 
-	}
-	public static void sortByGPA() 
-	{
-		Collections.sort(DataOfStudent.studentInformation, new GPASorter()); 
-	}
-	public static void sortByClass() 
-	{
-		// sort by class 
-	}
+	//asdfasdf
 }
