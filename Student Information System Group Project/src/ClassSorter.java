22ac32c28b7faaec0111b3f95a6dd89b15b2cd8a
tointonst <tointonst@mullenmail.com>
2014-10-23 08:05:03 -0600

    Update ClassSorter.java

--- Student Information System Group Project/src/ClassSorter.java
+++ Student Information System Group Project/src/ClassSorter.java
@@ -4,9 +4,8 @@ public class ClassSorter implements Comparator<Student>
 	{
 	public int compare(Student s1, Student s2)
 		{
-		if (userChoice ==1 ) return s1.getPeriodOne().compareTo(s2.getPeriodOne());
-		else if (userChoice ==2) return s1.getPeriodTwo().compareTo(s2.getPeriodTwo());
+		if (SortStudents.userPeriodSort ==1 ) return s1.getPeriodOne().compareTo(s2.getPeriodOne());
+		else if (SortStudents.userPeriodSort ==2) return s1.getPeriodTwo().compareTo(s2.getPeriodTwo());
 		else return s1.getPeriodThree().compareTo(s2.getPeriodThree());
-		//dfad
 		}
 	}
